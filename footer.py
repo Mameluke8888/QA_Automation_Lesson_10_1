cd4c7b26becd0e05e69331b59807174688db8877
Evgeny Abdulin <evgenyabdulin@Evgenys-Mac-mini.local>
2021-05-24 04:25:51 -0500

    footer.py added methods for all clickable elements

--- footer.py
+++ footer.py
@@ -1,8 +1,9 @@
 from UIElement import UIElement as Element
 from selenium.webdriver.common.by import By
 
-# May 20th, 2021
+# May 23rd, 2021
 # student Evgeny Abdulin
+
 
 class Footer:
     def __init__(self, browser):
@@ -21,7 +22,7 @@ class Footer:
         self.brands = Element(browser, By.XPATH, "//a[contains(text(),'Brands')]")
         self.gift_certificates = Element(browser, By.XPATH, "//a[contains(text(),'Gift Certificates')]")
         self.affiliates = Element(browser, By.XPATH, "//a[contains(text(),'Affiliates')]")
-        
+
         self.extras_title = Element(browser, By.XPATH, "// h5[contains(., 'My Account')]")
         self.my_account = Element(browser, By.XPATH, "//a[contains(text(),'My Account')]")
         self.order_history = Element(browser, By.XPATH, "//a[contains(text(),'Order History')]")
@@ -30,7 +31,44 @@ class Footer:
 
         self.opencart_ext_link = Element(browser, By.XPATH, "//a[contains(text(),'OpenCart')]")
 
-
     def click_about_us(self):
         self.about_us.click()
 
+    def click_delivery_info (self):
+        self.delivery_info .click()
+
+    def click_privacy_policy(self):
+        self.privacy_policy.click()
+
+    def click_terms_conditions(self):
+        self.terms_conditions.click()
+
+    def click_contact_us(self):
+        self.contact_us.click()
+
+    def click_returns(self):
+        self.returns.click()
+
+    def click_site_map(self):
+        self.site_map.click()
+
+    def click_brands(self):
+        self.brands.click()
+
+    def click_gift_certificates(self):
+        self.gift_certificates.click()
+
+    def click_my_account(self):
+        self.my_account.click()
+
+    def click_order_history(self):
+        self.order_history.click()
+
+    def click_wish_list(self):
+        self.wish_list.click()
+
+    def click_newsletter(self):
+        self.newsletter.click()
+
+    def click_opencart_ext_link(self):
+        self.opencart_ext_link.click()
